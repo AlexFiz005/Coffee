15c62b9aa9b53710c09fcf4db65ccd1381608217
Alex Fiz <alexf.webdev@gmail.com>
2023-11-03 14:07:15 +0300

    Первый коммит.

---
+++ Coffee/Controllers/AdminController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Coffee.Controllers
+{
+    [Authorize(Roles = "Administrator")]
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    
+        public IActionResult Users()
+        {
+            var ListUsers = new List<string>();
+
+            return View();
+        }
+    }
+}
