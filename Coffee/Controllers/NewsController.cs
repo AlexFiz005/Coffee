75438a099424d22e121d830c5433b813f4e17a37
Alex Fiz <alexf.webdev@gmail.com>
2023-11-07 12:45:16 +0300

    Добавил cshtml, css с bootstrap.

---
+++ Coffee/Controllers/NewsController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Coffee.Controllers
+{
+    public class NewsController : Controller
+    {
+        public IActionResult News()
+        {
+            return View();
+        }
+    }
+}
+
